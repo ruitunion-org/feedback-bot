dc621096032f7942b0a58f635a3508b4ad6169a4
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2025-11-24 19:07:57 +0300

    Improve id resolver

--- RuItUnion.FeedbackBot/Services/ReplyUserIdAdvancedResolver.cs
+++ RuItUnion.FeedbackBot/Services/ReplyUserIdAdvancedResolver.cs
@@ -1,4 +1,5 @@
-﻿using TgBotFrame.Commands.Authorization.Services;
+﻿using Telegram.Bot.Types.Enums;
+using TgBotFrame.Commands.Authorization.Services;
 
 namespace RuItUnion.FeedbackBot.Services;
 
@@ -6,20 +7,43 @@ public class ReplyUserIdAdvancedResolver(IFeedbackBotContext db) : ReplyUserIdRe
 {
     public override async ValueTask<long?> GetReplyUserId(Update update, CancellationToken ct = default)
     {
-        if (update.Message?.ReplyToMessage?.From?.IsBot == true && update.Message.MessageThreadId is not null)
+        if (update.Message?.ReplyToMessage?.From?.IsBot != true || update.Message.MessageThreadId is null)
         {
-            string? name = update.Message.ReplyToMessage.ForwardSenderName;
-            long userId = await db.Topics
-                .AsNoTracking()
-                .Where(x => x.ThreadId == update.Message.MessageThreadId
-                            && (x.User.LastName == null
-                                ? x.User.FirstName
-                                : x.User.FirstName + @" " + x.User.LastName) == name)
-                .Select(x => x.User.Id)
-                .FirstOrDefaultAsync(ct).ConfigureAwait(false);
-            return userId == 0 ? null : userId;
+            return update.Message?.ReplyToMessage?.From?.Id;
         }
 
-        return update.Message?.ReplyToMessage?.From?.Id;
+        long userId = 0;
+        switch (update.Message?.ReplyToMessage?.ForwardOrigin?.Type)
+        {
+            case MessageOriginType.Chat:
+                userId = ((MessageOriginChat)update.Message.ReplyToMessage.ForwardOrigin).SenderChat.Id;
+                break;
+            case MessageOriginType.Channel:
+                userId = ((MessageOriginChannel)update.Message.ReplyToMessage.ForwardOrigin).Chat.Id;
+                break;
+            case MessageOriginType.User:
+                userId = ((MessageOriginUser)update.Message.ReplyToMessage.ForwardOrigin).SenderUser.Id;
+                break;
+            case MessageOriginType.HiddenUser:
+                if (update.Message?.MessageThreadId is not null
+                    && update.Message.ReplyToMessage?.Type is < MessageType.ForumTopicCreated
+                        or > MessageType.GeneralForumTopicUnhidden
+                    && update.Message?.ReplyToMessage?.From?.IsBot == true
+                    && update.Message.ReplyToMessage.ForwardOrigin is not null)
+                {
+                    userId = await db.Topics
+                        .Where(x => x.ThreadId == update.Message.MessageThreadId)
+                        .Select(x => x.UserChatId)
+                        .FirstOrDefaultAsync(ct).ConfigureAwait(false);
+                }
+
+                break;
+            case null:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        return userId == 0 ? null : userId;
     }
 }
